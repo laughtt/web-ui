c7be07e8f68c8d1a2d7035fb0a867943f533a325
mauricio <jcarpioherrera@gmail.com>
2025-03-20 15:05:42 +0800

    change pass

--- src/utils/mongodb.py
+++ src/utils/mongodb.py
@@ -20,7 +20,7 @@ class MongoDB:
     def connect(self):
         """Connect to MongoDB using environment variables"""
         try:
-            mongo_uri = f"mongodb://{os.getenv('MONGODB_ROOT_USERNAME')}:{os.getenv('MONGODB_ROOT_PASSWORD')}@mongodb:{os.getenv('MONGODB_PORT')}/?authSource=admin"
+            mongo_uri = f"mongodb://{os.getenv('MONGODB_ROOT_USERNAME')}:{os.getenv('MONGODB_ROOT_PASSWORD')}@mongodb:27017/"
             self.mongo_uri = mongo_uri
             # Create MongoDB client
             print(mongo_uri)
